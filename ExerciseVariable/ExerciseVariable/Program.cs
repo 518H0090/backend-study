db17d966a660195ab681ec9c61a40837cc8d4b73
518H0090 <nhoxhieuro5@gmail.com>
2025-09-05 08:00:58 +0700

    Initial Step

---
+++ ExerciseVariable/ExerciseVariable/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int speed = 5;
+Console.WriteLine(speed);
+
+int age = 20;
+int ageDouble = age * 2;
+
+Console.WriteLine(ageDouble == (age * 2));
